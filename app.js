9053a39eaf83224a6fcade851536889dec18f7b4
Sheila Anguiano <sheila.anguiano@gmail.com>
2019-12-09 23:09:26 -0800

    Updated app.js

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
-//Problem: We need a simple way to look at a user's badge count and JavaScript point from a web browser
+const router = require("./router.js");
 
+//Problem: We need a simple way to look at a user's badge count and JavaScript point from a web browser
 //Solution: Use node.js to perform the profile look ups and serve our template via HTTP
 
-//1. Create a web server
+//Create a web server
 const http = require('http');
 
 const hostname = '127.0.0.1';
 const port = 8080;
 
 const nodeServer = http.createServer((request, response) => {
-    homeRoute(request, response);
+    router.home(request, response);
+    router.user(request, response);
   
 });
 
@@ -17,30 +19,6 @@ nodeServer.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
 
-//2. Handle HTTO route GET / and POST  i.e. Home
-function homeRoute(request, response){
-    //if the url == "/" && GET
-    if(request.url === "/") {
-        //show search
-        response.statusCode = 200;
-        response.setHeader('Content-Type', 'text/plain');
-        response.write("header\n");
-        response.write("search\n");
-        response.end("footer\n");
-    }
-
-    //if url == "/" && POST 
-        //redirect to /:username
-}
-
-//3. Handle HTTP route GET /:username i.e. /sheilaanguiano
-    //if url =="/.."
-        //get json from Treehouse
-    //on "end"
-        //show profile
-    //on "error"
-        //show error
-
-//4.Function that handles the reading of iles and merge in value
+//Function that handles the reading of iles and merge in value
     //read from file and get a string
         //merge values in to string
