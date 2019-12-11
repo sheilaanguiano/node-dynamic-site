58194007797996852129b3c8be2e8319e0aa4e9b
Sheila Anguiano <sheila.anguiano@gmail.com>
2019-12-10 22:29:53 -0800

    restored working web server

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-const router = require("./router.js");
-
 //Problem: We need a simple way to look at a user's badge count and JavaScript point from a web browser
 //Solution: Use node.js to perform the profile look ups and serve our template via HTTP
 
@@ -9,16 +7,30 @@ const http = require('http');
 const hostname = '127.0.0.1';
 const port = 8080;
 
-const nodeServer = http.createServer((request, response) => {
-    router.home(request, response);
-    router.user(request, response);
-  
+const server = http.createServer((request, response) => {
+  response.statusCode = 200;
+  response.setHeader('Content-Type', 'text/plain');
+  response.end('Hello, World!\n');
 });
 
-nodeServer.listen(port, hostname, () => {
+server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
 
-//Function that handles the reading of iles and merge in value
+//2. Handle HTTP route GET / and POST  i.e. Home
+    //if the url == "/" && GET
+        //show search
+    //if url == "/" && POST 
+        //redirect to /:username
+
+//3. Handle HTTP route GET /:username i.e. /sheilaanguiano
+    //if url =="/.."
+        //get json from Treehouse
+    //on "end"
+        //show profile
+    //on "error"
+        //show error
+
+//4.Function that handles the reading of iles and merge in value
     //read from file and get a string
         //merge values in to string
