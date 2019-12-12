0f8bf6786be830815f80ba5dccedb7605afa977b
Sheila Anguiano <sheila.anguiano@gmail.com>
2019-12-11 21:03:33 -0800

    const Profile to router.js to populate info

--- router.js
+++ router.js
@@ -1,3 +1,8 @@
+const Profile = require("./profile.js")
+
+//-----------------------------------
+//  HOME ROUTE
+//-----------------------------------
 //Handle HTTP route GET / and POST  i.e. Home
 function home(request, response) {
     //if the url == "/" && GET
@@ -14,6 +19,9 @@ function home(request, response) {
         //redirect to /:username
 }
 
+//-----------------------------------
+//  USER ROUTE
+//-----------------------------------
 //Handle HTTP route GET /:username i.e. /sheilaanguiano
 function user(request, response) {
     //if url =="/.."
@@ -22,14 +30,32 @@ function user(request, response) {
         response.statusCode = 200;
         response.setHeader('Content-Type', 'text/plain');
         response.write("header\n");
-        response.write(username + "\n");
-        response.end("footer\n");
-    }
+
         //get json from Treehouse
-    //on "end"
-        //show profile
+        const studentProfile = new Profile(username);
+        
+        //on "end"
+        studentProfile.on("end", function(profileJSON) {
+            //show profile
+
+            //store the values which we need
+            const values = {
+                avatarUrl:profileJSON.gravatar_url,
+                username:profileJSON.profile_name,
+                badges:profileJSON.badges.length,
+                javascriptPoints:profileJSON.points.JavaScript
+            }
+            //Simple Response
+            response.write(values.username + " has " + values.badges +   " badges\n");
+            response.end("footer\n");
+        });
+    
     //on "error"
+    studentProfile.on("error", function(error) {
         //show error
+        response.end("fooooooter\n");
+        });        
+    }
 }
 
 module.exports.home = home;
